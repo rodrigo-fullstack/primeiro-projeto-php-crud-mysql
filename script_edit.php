b98ce8fa5b609f52944683abe7eec63cf6d80da0
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-09-24 19:48:10 -0300

    feat(update): Alteração dos dados correções

--- script_edit.php
+++ script_edit.php
@@ -1,19 +1,45 @@
-<?php
-  include "script_conexao.php";
-  
-  $id = $_POST("id");
-  $nome = $_POST("nome");
-  $email = $_POST("email");
-  $telefone = $_POST("tel");
-  $endereco = $_POST("endereco");
-  $data_nascimento = $_POST("data_nascimento");
-  
-  $sql = "UPDATE ˋpessoasˋ set `nome` = '$nome', `email` = '$email', `telefone` = '$telefone', `endereco` = '$endereco', `data_nascimento` = '$data_nascimento' WHERE id_pessoa = '$id'";
-  
-    mysqli_query($conn, $sql);
-  
-    echo "$id, $nome, $email, $telefone, $endereco, $data_nascimento";
-  
-  
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Script</title>
+    <link rel="stylesheet" href="ASSETS/CSS/main.css">
+    <link rel="stylesheet" href="ASSETS/CSS/messages.css">
+</head>
+<body>
+    <div class="container">
+      	<?php
+			include "script_conexao.php";
+			include "messages.php";
+
+			$id = $_POST["id"];
+			$nome = $_POST["nome"];
+			$email = $_POST["email"];
+			$telefone = $_POST["tel"];
+			$endereco = $_POST["endereco"];
+			$data_nascimento = $_POST["data_nascimento"];
+			
+			// Atualizando no BD
+			$sql = "UPDATE pessoas set nome = '$nome', email = '$email', telefone = '$telefone', endereco = '$endereco', data_nascimento = '$data_nascimento' WHERE id_pessoa = '$id'";
+			
+			if(mysqli_query($conn, $sql)){
+				mensagem("$nome ALTERADO com Sucesso!", "success");
+			} else{
+				mensagem("$nome NÃO foi alterado!","error");
+			};
+		
+      
+  ?>
+
+        <div class="btn-container">
+            <a href="pesquisa.php" class="btn btn-return">Voltar</a>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
 
