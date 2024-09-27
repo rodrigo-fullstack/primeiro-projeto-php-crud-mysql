c6d316bbec5bdf049f2e34bac6d14c5c1fc270dc
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-09-27 16:56:13 -0300

    fix: Nome da foto 02

--- trabalhando_fotos.php
+++ trabalhando_fotos.php
@@ -7,11 +7,15 @@
         if(!$vetor_arquivo['error'] && $tipo_arquivo == "image"){
             
             $extensao_foto = $vetor_tipo_mime[1];
-            $nome_foto = md5($vetor_arquivo['name']) . ".$extensao_foto";
+            
+            $nome_foto = md5(date('ymdhms') . $vetor_arquivo['name']) . ".$extensao_foto";
+            
             //$_SERVER['DOCUMENT_ROOT'] realiza a captura da pasta raiz de execução do PHP
             $pasta_foto = $_SERVER['DOCUMENT_ROOT'] . '/BackEndPHP/PHP_CRUD_MySQL/ASSETS/IMG/';
 
-            move_uploaded_file($vetor_arquivo['tmp_name'], $pasta_foto . $nome_foto);
+            $tmp = $vetor_arquivo['tmp_name'];
+            
+            move_uploaded_file($tmp, $pasta_foto . $nome_foto);
     
             return $nome_foto;
         } else if($vetor_arquivo["error"]){
